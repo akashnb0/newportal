f5dbffa6c24fb4946864cdda500308cbee2375a8
Akash S <akashajay510@gmail.com>
2024-06-13 00:09:41 +0530

    newport

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Grid, Box, Button } from '@mui/material';
+import styled from 'styled-components';
+import Students from "../assets/students.svg";
+import { LightPurpleButton } from '../components/buttonStyles';
+
+const Homepage = () => {
+    return (
+        <StyledContainer>
+            <Grid container spacing={0}>
+                <Grid item xs={12} md={6}>
+                    <img src={Students} alt="students" style={{ width: '100%' }} />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <StyledPaper elevation={3}>
+                        <StyledTitle>
+                            Welcome to
+                            <br />
+                            Saraswathi Technical Institute
+                            <br />
+                            'COA Portal'
+                        </StyledTitle>
+                        <StyledText>
+                            This portal has been exclusively created for the Computer on Office Automation candidates of Saraswathi Technical Institute. Feel free to explore our new portal!
+                        </StyledText>
+                        <StyledBox>
+                            <StyledLink to="/choose">
+                                <LightPurpleButton variant="contained" fullWidth>
+                                    Login
+                                </LightPurpleButton>
+                            </StyledLink>
+                            <StyledLink to="/choose">
+                                <Button variant="outlined" fullWidth
+                                    sx={{ mt: 2, mb: 3, color: "#7f56da", borderColor: "#7f56da" }}
+                                >
+                                    Login as Guest
+                                </Button>
+                            </StyledLink>
+                            <StyledText>
+                                Don't have an account?{' '}
+                                <a href="tel:8220093377" style={{ color: "#550080" }}>
+                                    Contact Admin
+                                </a>
+                            </StyledText>
+                        </StyledBox>
+                    </StyledPaper>
+                </Grid>
+            </Grid>
+        </StyledContainer>
+    );
+};
+
+export default Homepage;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledPaper = styled.div`
+  padding: 24px;
+  height: 100vh;
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content:center;
+  gap: 16px;
+  padding: 24px;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 3rem;
+  color: #252525;
+  /* font-family: "Manrope"; */
+  font-weight: bold;
+  padding-top: 0;
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledText = styled.p`
+  /* color: #550080; */
+  margin-top: 30px;
+  margin-bottom: 30px; 
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
